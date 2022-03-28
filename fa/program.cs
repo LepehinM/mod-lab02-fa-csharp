ceb7b014ac200af3a6d321c27452801e4e02c199
Makar <anl-pr@yandex.ru>
2022-03-28 11:43:07 +0300

    comment

--- fa/program.cs
+++ fa/program.cs
@@ -4,16 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace fans
-{
-    public class State
-    {
-        public string Name;
-        public Dictionary<char, State> Transitions;
-        public bool IsAcceptState;
-    }
-    public class FA1
-    {
+namespace fans {
+  public class State {
+    public string Name;
+    public Dictionary<char, State> Transitions;
+    public bool IsAcceptState;
+  }
+  public class FA1
+  {
         public static State a = new State()
         {
             Name = "a",
@@ -46,35 +44,35 @@ namespace fans
         };
 
         State InitialState = a;
-
+        
         public FA1()
         {
-            a.Transitions['0'] = c;
-            a.Transitions['1'] = b;
-            b.Transitions['0'] = d;
-            b.Transitions['1'] = b;
-            c.Transitions['0'] = e;
-            c.Transitions['1'] = d;
-            d.Transitions['0'] = e;
-            d.Transitions['1'] = d;
-            e.Transitions['0'] = e;
-            e.Transitions['1'] = e;
+           a.Transitions['0'] = c;
+           a.Transitions['1'] = b;
+           b.Transitions['0'] = d;
+           b.Transitions['1'] = b;
+           c.Transitions['0'] = e;
+           c.Transitions['1'] = d;
+           d.Transitions['0'] = e;
+           d.Transitions['1'] = d;
+           e.Transitions['0'] = e;
+           e.Transitions['1'] = e;         
         }
-
+        
         public bool? Run(IEnumerable<char> s)
         {
             State current = InitialState;
             foreach (var c in s) // цикл по всем символам 
             {
                 current = current.Transitions[c];
-                if (current == null)
+                if (current == null)              
                     return null;
             }
             return current.IsAcceptState;
         }
-    }
-    public class FA2
-    {
+  }
+  public class FA2
+  {
         public static State a = new State()
         {
             Name = "a",
@@ -100,19 +98,19 @@ namespace fans
             Transitions = new Dictionary<char, State>()
         };
         State InitialState = a;
-
+        
         public FA2()
         {
-            a.Transitions['0'] = c;
-            a.Transitions['1'] = b;
-            b.Transitions['0'] = d;
-            b.Transitions['1'] = a;
-            c.Transitions['0'] = a;
-            c.Transitions['1'] = d;
-            d.Transitions['0'] = b;
-            d.Transitions['1'] = c;
+           a.Transitions['0'] = c;
+           a.Transitions['1'] = b;
+           b.Transitions['0'] = d;
+           b.Transitions['1'] = a;
+           c.Transitions['0'] = a;
+           c.Transitions['1'] = d;
+           d.Transitions['0'] = b;
+           d.Transitions['1'] = c;     
         }
-
+        
         public bool? Run(IEnumerable<char> s)
         {
             State current = InitialState;
@@ -125,9 +123,9 @@ namespace fans
             }
             return current.IsAcceptState;         // результат true если в конце финальное состо€ние 
         }
-    }
-    public class FA3
-    {
+  }
+public class FA3
+  {
         public static State a = new State()
         {
             Name = "a",
@@ -147,38 +145,38 @@ namespace fans
             Transitions = new Dictionary<char, State>()
         };
         State InitialState = a;
-
+        
         public FA3()
         {
-            a.Transitions['0'] = a;
-            a.Transitions['1'] = b;
-            b.Transitions['0'] = a;
-            b.Transitions['1'] = c;
-            c.Transitions['0'] = c;
-            c.Transitions['1'] = c;
+           a.Transitions['0'] = a;
+           a.Transitions['1'] = b;
+           b.Transitions['0'] = a;
+           b.Transitions['1'] = c;
+           c.Transitions['0'] = c;
+           c.Transitions['1'] = c;     
         }
-
+        
         public bool? Run(IEnumerable<char> s)
         {
             State current = InitialState;
             foreach (var c in s)
             {
-                current = current.Transitions[c];
+                current = current.Transitions[c]; 
                 if (current == null)
                     return null;
                 // иначе переходим к следующему
             }
             return current.IsAcceptState;         // результат true если в конце финальное состо€ние 
         }
-    }
-    class Program
+  }
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            String s = "0000010111";
-            FA fa = new FA();
-            bool? result = fa.Run(s);
-            Console.WriteLine(result);
-        }
+      String s = "0000010111";
+      FA2 fa = new FA2();
+      bool? result = fa.Run(s);
+      Console.WriteLine(result);
     }
+  }
 }
